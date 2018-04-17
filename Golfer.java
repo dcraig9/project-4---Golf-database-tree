96cce19f0ea318934dcca50f4b67ee31ec64b7fb
dcraig9 <36308131+dcraig9@users.noreply.github.com>
2018-04-17 11:02:01 -0400

    Add files via upload

---
+++ Golfer.java
@@ -0,0 +1,172 @@
+/********************************************************************************************
+* File: Golfer.java
+* Golfer class simulates a Golfer and his related scores(name, average, rounds played, handicap)
+*
+* Authors: Donald Craig and Joe Eckstein
+* Date: 04/17/2018
+*
+********************************************************************************************/
+
+
+class Golfer {
+
+
+   private String lastname;
+   private int numberOfRounds;
+   private double averageScore;
+   private int handicap;
+
+   /**
+   * Initialize a 
+   * @param 
+   * 
+   **/
+   public Golfer()
+   {
+      //default constructor
+   }
+
+   /**
+   * Initialize a Golfer 
+   * @param 
+   * 
+   * @param 
+   *    
+   * @param 
+   *    
+   * @param 
+   *    
+   * @postcondition 
+   *    
+   **/
+   public Golfer(String name, int rounds, double average, int handicap)
+   {
+   
+   
+   
+   
+   
+   }
+   
+   /**
+   * Accessor method    
+   * @param - none
+   * @return
+   *   
+   **/
+   public int getName()
+   {
+   
+   }
+   
+   /**
+   * Modification method 
+   * @param - 
+   * @postcondition
+   *   
+   *   
+   **/   
+   public void setName(string name)
+   {
+   
+   }
+   
+   /**
+   * Accessor method    
+   * @param - none
+   * @return
+   *   
+   **/   
+   public int getRounds()
+   {
+   
+   }
+   
+   /**
+   * Modification method 
+   * @param - 
+   * @postcondition
+   *   
+   *   
+   **/   
+   public void setRounds(int rounds)
+   {
+   
+   }
+   
+   /**
+   * Accessor method    
+   * @param - none
+   * @return
+   *   
+   **/   
+   public double getAverage()
+   {
+   
+   }
+   
+   /**
+   * Modification method 
+   * @param - 
+   * @postcondition
+   *   
+   *   
+   **/   
+   public void setAverage(double average)
+   {
+   
+   }
+   
+   /**
+   * Modification method 
+   * @param - 
+   * @postcondition
+   *   
+   *   
+   **/   
+   public int setHandicap(int num)
+   {
+   
+   }
+   /**
+   * Accessor method    
+   * @param - none
+   * @return
+   *   
+   **/   
+   public void getHandicap()
+   {
+   
+   }
+   
+   public void addNewScore(int newScore)
+   {
+   
+   }
+   
+   public int compareTo (Golfer test)
+   {
+   
+      return 0;
+   }
+   
+   public String toString(Golfer source)
+   {
+      String output;
+      
+      return output;
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+
+}// end class
